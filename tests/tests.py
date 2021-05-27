c7a7f2c36963cf9f8635107955676bb24da4ca64
Montyblack <47610523+Montyblack@users.noreply.github.com>
2021-05-27 23:29:25 +0300

    new commit

--- tests/tests.py
+++ tests/tests.py
@@ -4,27 +4,17 @@ import numpy as np
 from skimage import util
 from skimage.transform import rescale
 import utils.graphic_calcs as ugc
-# from mp_colors.crop import FaceCropper
+
+from ../src import get_colortypes_csv
 
 from get_colortypes_csv import \
     get_landmarks, \
     get_face_undereyes_color, \
     get_face_forehead_color, \
     get_face_cheeks_color, get_face_hair_color
-# , \
-#     get_lips_mediapipe, \
-#     get_cheek_mediapipe, \
-#     get_forehead_mediapipe, \
-#     get_nose_mediapipe, \
-#     get_skin_mediapipe, \
-#     get_eyes_mediapipe, \
-#     get_iris_mediapipe
 
-# from mp_colors.colors import get_keypoints
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-# face_cropper = FaceCropper(alpha=0.4)
 
 
 class Case1(unittest.TestCase):
@@ -36,12 +26,6 @@ class Case1(unittest.TestCase):
         self.landmarks, _ = get_landmarks(self.image, '00000428.jpg')
 
         _, _, _, self.imageRGB = get_face_hair_color(self.imageBGR)
-#         self.faces = face_cropper.detect(self.imageRGB)
-#         self.face = self.faces[0]
-#         self.mp_keypoints = np.load('./mp_keypoints_1.npy', allow_pickle=False)
-
-    # def test_keypoints(self):
-    #     self.assertEqual((np.abs(self.face - np.load('./face.npy'))).sum(), 0)
 
     def test_get_face_undereyes_color(self):
         face_undereyes_color_h, face_undereyes_color_s, \
@@ -71,38 +55,3 @@ class Case1(unittest.TestCase):
             face_hair_color_v, _ = get_face_hair_color(self.imageBGR)
         self.assertEqual([face_hair_color_h, face_hair_color_s, face_hair_color_v],
                          [0.0, 0.0, 1.0])
-
-#     def test_get_lips_mediapipe(self):
-#         mp_lip_r, mp_lip_g, \
-#             mp_lip_b = get_lips_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_lip_r, mp_lip_g, mp_lip_b], [192, 124, 124])
-
-#     def test_get_cheek_mediapipe(self):
-#         mp_cheek_r, mp_cheek_g, \
-#             mp_cheek_b = get_cheek_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_cheek_r, mp_cheek_g, mp_cheek_b], [224, 179, 161])
-
-#     def test_get_forehead_mediapipe(self):
-#         mp_forehead_r, mp_forehead_g, \
-#             mp_forehead_b = get_forehead_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_forehead_r, mp_forehead_g, mp_forehead_b], [222, 185, 166])
-
-#     def test_get_nose_mediapipe(self):
-#         mp_nose_r, mp_nose_g, \
-#             mp_nose_b = get_nose_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_nose_r, mp_nose_g, mp_nose_b], [222, 187, 170])
-
-#     def test_get_skin_mediapipe(self):
-#         mp_skin_r, mp_skin_g, \
-#             mp_skin_b = get_skin_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_skin_r, mp_skin_g, mp_skin_b], [223, 183, 165])
-
-#     def test_get_eyes_mediapipe(self):
-#         mp_eyes_r, mp_eyes_g, \
-#             mp_eyes_b = get_eyes_mediapipe(self.face, self.mp_keypoints)
-#         self.assertEqual([mp_eyes_r, mp_eyes_g, mp_eyes_b], [100, 81, 73])
-
-#     def test_get_iris_mediapipe(self):
-#         mp_iris_r, mp_iris_g, \
-#             mp_iris_b = get_iris_mediapipe(self.face)
-#         self.assertEqual([mp_iris_r, mp_iris_g, mp_iris_b], [35.0, 43.0, 58.0])
